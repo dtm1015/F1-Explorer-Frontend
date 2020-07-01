b3f0a48d7121354502e12b5f1afd3f95af1ea7e6
DTM1015 <dtm1015@vt.edu>
2020-07-01 13:47:09 -0400

    minor changes to page layout

---
+++ components/CircuitInfo.js
@@ -0,0 +1,9 @@
+function CircuitInfo(){
+  return (
+      <div id="InfoContainer">
+          <span id="prompt"> Select a circuit to view Info </span>
+      </div>
+  )
+}
+
+export default CircuitInfo;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import CircuitMap from '../components/CircuitMap'
+import CircuitInfo from '../components/CircuitInfo'
 import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div className="Home">
       <Header/>
-      
+
       <CircuitMap/>
+      <CircuitInfo/>
 
       <Footer/>
     </div>
